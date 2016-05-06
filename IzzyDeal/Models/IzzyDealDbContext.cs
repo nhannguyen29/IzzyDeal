18bf32517a27039e505ee871ead0b97c1e50f9c8
nhn29 <nhn29@drexel.edu>
2016-05-06 19:28:06 -0400

    Add DbContext

---
+++ IzzyDeal/Models/IzzyDealDbContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace IzzyDeal.Models
+{
+    public class IzzyDealDbContext : DbContext
+    {
+        // public IzzyDealContext()
+        // {
+        //     Database.EnsureCreated();
+        // }
+        
+        public DbSet<Deal> Deals { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<StoreLocation> StoreLocations { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var path = PlatformServices.Default.Application.ApplicationBasePath;
+            optionsBuilder.UseSqlite("Filename=" + Path.Combine(path, "IzzyDeal.db"));
+        }
+    }
+    
+}
