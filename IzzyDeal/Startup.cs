00c186cc062548ac137870079dcf64a935f97b65
nhn29 <nhn29@drexel.edu>
2016-06-19 17:48:37 -0400

    Add services  to Startup

--- IzzyDeal/Startup.cs
+++ IzzyDeal/Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
 using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using IzzyDeal.Models;
-using IzzyDeal.Services;
+using Newtonsoft.Json.Serialization;
 
 namespace IzzyDeal
 {
@@ -23,12 +18,6 @@ namespace IzzyDeal
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-            if (env.IsDevelopment())
-            {
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
-            }
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
@@ -44,25 +33,26 @@ namespace IzzyDeal
             // Add framework services.
             services.AddEntityFramework()
                 .AddSqlite()
-                .AddDbContext<ApplicationDbContext>(options =>
+                .AddDbContext<IzzyDealDbContext>(options =>
                     options.UseSqlite(Configuration["Data:DefaultConnection:ConnectionString"]));
+            
+            services.AddScoped<IzzyDealSeedData>();
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            services.AddScoped<IIzzyDealRepository, IzzyDealRepository>();
 
-            services.AddMvc();
+            services.AddLogging();
+            services.AddMvc()
+                    .AddJsonOptions(opt =>
+                    {
+                       opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); 
+                    });
 
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IzzyDealSeedData seeder, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddDebug(LogLevel.Warning);
 
             if (env.IsDevelopment())
             {
@@ -79,7 +69,7 @@ namespace IzzyDeal
                     using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
                         .CreateScope())
                     {
-                        serviceScope.ServiceProvider.GetService<ApplicationDbContext>()
+                        serviceScope.ServiceProvider.GetService<IzzyDealDbContext>()
                              .Database.Migrate();
                     }
                 }
@@ -90,7 +80,7 @@ namespace IzzyDeal
 
             app.UseStaticFiles();
 
-            app.UseIdentity();
+            
 
             // To configure external authentication please see http://go.microsoft.com/fwlink/?LinkID=532715
 
@@ -100,6 +90,8 @@ namespace IzzyDeal
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            
+            seeder.EnsureSeedData();
         }
 
         // Entry point for the application.
